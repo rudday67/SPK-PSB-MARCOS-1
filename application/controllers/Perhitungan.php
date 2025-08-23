53ddc3a72bd9540c1ba29ece57b5e1c05899da02
rudday67 <rudysatya02@gmail.com>
2025-08-23 22:30:39 +0800

    Update Perhitungan.php
    update

--- application/controllers/Perhitungan.php
+++ application/controllers/Perhitungan.php
@@ -1,47 +1,60 @@
 <?php
-    
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    
-    class Perhitungan extends CI_Controller {
-    
-        public function __construct()
-        {
-            parent::__construct();
-            $this->load->library('pagination');
-            $this->load->library('form_validation');
-            $this->load->model('Perhitungan_model');
-        }
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-        public function index()
-        {
-            if ($this->session->userdata('id_user_level') != "1") {
-            ?>
-				<script type="text/javascript">
-                    alert('Anda tidak berhak mengakses halaman ini!');
-                    window.location='<?php echo base_url("Login/home"); ?>'
-                </script>
-            <?php
-			}
-			
-			$data = [
-                'page' => "Perhitungan",
-                'kriterias'=> $this->Perhitungan_model->get_kriteria(),
-                'alternatifs'=> $this->Perhitungan_model->get_alternatif(),
-            ];
-			
-            $this->load->view('perhitungan/perhitungan', $data);
+class Perhitungan extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('pagination');
+        $this->load->library('form_validation');
+        $this->load->model('Perhitungan_model');
+    }
+
+    // FUNGSI INDEX INI TETAP UNTUK ADMIN
+    public function index()
+    {
+        if ($this->session->userdata('id_user_level') != "1") {
+        ?>
+            <script type="text/javascript">
+                alert('Anda tidak berhak mengakses halaman ini!');
+                window.location='<?php echo base_url("Login/home"); ?>'
+            </script>
+        <?php
         }
-		
-		public function hasil()
-        {
-            $data = [
-                'page' => "Hasil",
-				'hasil'=> $this->Perhitungan_model->get_hasil()
-            ];
-			
-            $this->load->view('perhitungan/hasil', $data);
-        }
-    
+        
+        $data = [
+            'page' => "Perhitungan",
+            'kriterias'=> $this->Perhitungan_model->get_kriteria(),
+            'alternatifs'=> $this->Perhitungan_model->get_alternatif(),
+        ];
+        
+        $this->load->view('perhitungan/perhitungan', $data);
     }
     
-    
+    // FUNGSI INI UNTUK ADMIN (TIDAK DIUBAH)
+    public function hasil()
+    {
+        $data = [
+            'page' => "Hasil",
+            'hasil'=> $this->Perhitungan_model->get_hasil()
+        ];
+        
+        // Memuat view 'hasil.php' yang menggunakan template admin
+        $this->load->view('perhitungan/hasil', $data);
+    }
+
+    // INI FUNGSI BARU YANG KITA TAMBAHKAN UNTUK PUBLIK
+    public function hasil_publik()
+    {
+        $data = [
+            'page' => "Hasil",
+            'hasil'=> $this->Perhitungan_model->get_hasil()
+        ];
+        
+        // Membungkus view 'hasil.php' dengan template publik
+        $this->load->view('layouts/header_publik', $data);
+        $this->load->view('perhitungan/hasil_publik', $data); // Kontennya pakai file yang sama
+        $this->load->view('layouts/footer_publik');
+    }
+}
